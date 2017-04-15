a7ee6cc6553c2f71864e4a4cf2ad2c0e36ad7e2a
uzair jawed <s.uzairjawed@gmail.com>
2017-04-15 13:27:57 +0100

    HTTP COLLECT solution added

---
+++ http-collect.js
@@ -0,0 +1,16 @@
+var http = require("http");
+var bl = require("bl");
+
+http.get(process.argv[2], function(response) {
+
+	response.pipe(bl(function(err, data) {
+
+		if (err) {
+			console.log(err);
+		}
+
+		console.log(data.length);
+		console.log(data.toString());	
+	}));
+
+});
